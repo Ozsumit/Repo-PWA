860f3b160f2d3516143c79ea6c9d806b29994780
Ozsumit <129023326+Ozsumit@users.noreply.github.com>
2024-09-24 07:15:29 +0545

    Update game.tsx

--- components/ui/game.tsx
+++ components/ui/game.tsx
@@ -742,7 +742,7 @@ const DonationClicker: React.FC = () => {
               <span>
                 <LucideIcons.Medal className="inline mr-2 text-yellow-600" />
               </span>
-              <span className="text-md">{achievement.name}</span>
+              <span >{achievement.name}</span>
             </button>
           ))}
         </div>
